49b4863312dc574a935e2530f255f751fc483852
GhadeerAbdulaziz <doodyag12@gmail.com>
2019-07-23 01:42:28 +0300

    create from scrach

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ghadeer_aziz\desktop\bootcamp7_7_django\task_01\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
